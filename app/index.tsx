e4f13fb36645a2dffda61c768c9021e40ba2a217
PROMLEE <donghoon099@naver.com>
2025-04-29 16:01:33 +0900

    chore: update dependencies in package.json
    - Added expo-linear-gradient version 14.0.2
    - Added moti version 0.30.0
    - Added expo-notifications version 0.29.14
    - Added expo-device version 7.0.3

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,50 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { router, useFocusEffect } from "expo-router";
-import { useCallback } from "react";
-import { Button, FlatList, RefreshControl, Text, View } from "react-native";
-const fetchUsers = async () => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return res.data;
-};
-export default function UserList() {
-  const { data, isLoading, isError, error, refetch, isRefetching } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchUsers,
-  });
+import React, { useReducer } from "react";
+import { StyleSheet, Pressable, Text } from "react-native";
+import { MotiView } from "moti";
+import { Skeleton } from "moti/skeleton";
 
-  useFocusEffect(
-    useCallback(() => {
-      refetch();
-    }, [])
-  );
+const Spacer = ({ height = 16 }) => <MotiView style={{ height }} />;
 
-  if (isLoading) return <Text>불러오는 중...</Text>;
-  if (isError) return <Text>오류 발생: {(error as Error).message}</Text>;
+export default function HelloWorld() {
+  const [dark, toggle] = useReducer((s) => !s, true);
+
+  const colorMode = dark ? "dark" : "light";
+
   return (
-    <FlatList
-      refreshControl={
-        <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
-      }
-      data={data}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <Text>{item.name}</Text>}
-      ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-      ListFooterComponent={() => (
-        <Button title="Next" onPress={() => router.push("/next")} />
-      )}
-    />
+    <Pressable onPress={toggle} style={styles.container}>
+      <MotiView
+        transition={{
+          type: "timing",
+        }}
+        style={[styles.container, styles.padded]}
+        animate={{ backgroundColor: dark ? "#000000" : "#ffffff" }}
+      >
+        <Skeleton
+          colorMode={colorMode}
+          radius="round"
+          height={75}
+          width={75}
+          show={colorMode === "dark"}
+        >
+          <Text>Hello</Text>
+        </Skeleton>
+        <Spacer />
+        <Skeleton colorMode={colorMode} width={250} />
+        <Spacer height={8} />
+        <Skeleton colorMode={colorMode} width={"100%"} />
+        <Spacer height={8} />
+        <Skeleton colorMode={colorMode} width={"100%"} />
+      </MotiView>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  padded: {
+    padding: 16,
+  },
+});
